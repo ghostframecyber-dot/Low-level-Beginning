60e87d8611264f33bf5ff78b6369f1da1078e730
Mr Frame <ghostframecyber@gmail.com>
2026-07-20 01:06:15 +0100

    adding the change been made just now

--- main.c
+++ main.c
@@ -1,16 +1,29 @@
 #include <stdio.h>
 
-// this is my first C program :) 
+// this is my first C program
 /*
-this is also comment
+this
+ is
+  also
+ comment
 */
-int main() {
-    printf("Hello, World!\n");
+
+/* int main() {
+    printf("Hello, World!\n"); 
+    printf("i like pizza \n");
     printf("Welcome to C programming.\n");
+    return 0;
+}*/ // \n for new line of character
 
-int age = 25;
-printf("you are %d years old", age);
+// variable
+// %d use in called {decimal}that in for variable
+int main() {
+    int age = 46;
+    int year = 2032;
+    float gpa = 4.34;
 
+    printf("The year is %d\n", year);
+    printf("You are %d years old\n", age);
+    printf("Your score is %f\n", gpa);
     return 0;
 }
-
--- readme.md
+++ readme.md
@@ -1,1 +1,5 @@
 how it started
+
+so this how to learn a language by every month, it's not hard as the you guys think it is
+
+RULE 1: start
